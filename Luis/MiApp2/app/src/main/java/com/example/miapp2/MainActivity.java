772543361898abf2756ef14c3ba61bc58bc877a3
lmonsalve <lmonsalve22@gmail.com>
2020-01-20 16:46:25 -0300

    Version preliminar

---
+++ Luis/MiApp2/app/src/main/java/com/example/miapp2/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.miapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.TargetApi;
+import android.os.Bundle;
+import android.webkit.WebResourceError;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebview ;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("https://anin.users.earthengine.app/view/appince");
+
+    }
+}
--- Luis/MiPrimerApp/app/src/main/java/com/example/miprimerapp/MainActivity.java
+++ Luis/MiPrimerApp/app/src/main/java/com/example/miprimerapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.miprimerapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -12,13 +13,18 @@ public class MainActivity extends AppCompatActivity {
     private EditText et1;
     private EditText et2;
     private TextView tv1;
-    
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "OnCreate", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, "OnCreate", Toast.LENGTH_SHORT).show();
+
+        et1 = (EditText)findViewById(R.id.txt_num1);
+        et1 = (EditText)findViewById(R.id.txt_num2);
+        tv1 = (TextView)findViewById(R.id.txt_resultado);
+
         // La actividad está a creada.
         /*
         int matematicas = 5;
@@ -36,6 +42,20 @@ public class MainActivity extends AppCompatActivity {
         }
         */
 
+    }
+
+    //Este método realiza la suma
+    public void Sumar(View view){
+        String valor1 = et1.getText().toString();
+        String valor2 = et1.getText().toString();
+
+        int num1 = Integer.parseInt(valor1);
+        int num2 = Integer.parseInt(valor2);
+
+        int suma = num1 + num2;
+
+        String result = String.valueOf(suma);
+        tv1.setText(result);
     }
     @Override
     protected void onStart() {
