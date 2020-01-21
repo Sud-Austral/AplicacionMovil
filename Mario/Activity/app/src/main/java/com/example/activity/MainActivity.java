30893e61bd771fd12ffa6b50c5eb5319e4868f0e
Mario9191 <mariosalazar9191@gmail.com>
2020-01-21 17:53:39 -0300

    .

--- Mario/Activity/app/src/main/java/com/example/activity/MainActivity.java
+++ Mario/Activity/app/src/main/java/com/example/activity/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("LOGCAT", "Entra en el metodo log onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("LOGCAT", "Entra en el metodo log onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("LOGCAT", "Entra en el metodo log onPause");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("LOGCAT", "Entra en el metodo log onDestroy");
     }
 }
