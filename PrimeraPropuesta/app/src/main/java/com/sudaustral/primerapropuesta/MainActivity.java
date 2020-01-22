34dad0b03132b20ad530b617f23c7093d4439e33
clentebanks <clentebanks0@gmail.com>
2020-01-22 16:03:31 -0600

    Update MainActivity.java

--- PrimeraPropuesta/app/src/main/java/com/sudaustral/primerapropuesta/MainActivity.java
+++ PrimeraPropuesta/app/src/main/java/com/sudaustral/primerapropuesta/MainActivity.java
@@ -3,6 +3,8 @@ package com.sudaustral.primerapropuesta;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
@@ -11,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//Pantalla completa
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);// NOTA:Este codigo elimina el titulo o el action bar en la APP
         setContentView(R.layout.activity_main);
         WebView myWebView = (WebView) findViewById(R.id.webview);
         WebSettings webSettings = myWebView.getSettings();
