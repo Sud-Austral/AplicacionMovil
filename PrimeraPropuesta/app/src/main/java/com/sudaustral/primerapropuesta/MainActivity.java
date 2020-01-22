0879b00068fb1f44bbfbdb63e5c411603e22ecfb
lmonsalve <lmonsalve22@gmail.com>
2020-01-22 10:37:20 -0300

    primera propuesta

---
+++ PrimeraPropuesta/app/src/main/java/com/sudaustral/primerapropuesta/MainActivity.java
@@ -0,0 +1,20 @@
+package com.sudaustral.primerapropuesta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("https://anin.users.earthengine.app/view/appince");
+    }
+}
