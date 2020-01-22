f450b83d07725a1fa7db49ca19ecb309cd3fb0c2
clentebanks <clentebanks0@gmail.com>
2020-01-22 12:27:42 -0600

    eliminar action bar

--- Luis/MiApp2/app/src/main/java/com/example/miapp2/MainActivity.java
+++ Luis/MiApp2/app/src/main/java/com/example/miapp2/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.TargetApi;
 import android.os.Bundle;
+import android.view.Window;
 import android.webkit.WebResourceError;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -15,11 +16,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);// NOTA:Este codigo elimina el titulo o el action bar en la APP
         setContentView(R.layout.activity_main);
         WebView myWebView = (WebView) findViewById(R.id.webview);
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        myWebView.loadUrl("https://anin.users.earthengine.app/view/appince");
+        myWebView.loadUrl("https://anin.users.earthengine.app/view/movil");
 
     }
 }
