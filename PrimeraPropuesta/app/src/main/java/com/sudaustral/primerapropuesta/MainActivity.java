591fa534078c8e65c12e034ab6194ff93bb62714
lmonsalve <lmonsalve22@gmail.com>
2020-01-23 12:59:32 -0300

    version beta

--- PrimeraPropuesta/app/src/main/java/com/sudaustral/primerapropuesta/MainActivity.java
+++ PrimeraPropuesta/app/src/main/java/com/sudaustral/primerapropuesta/MainActivity.java
@@ -2,11 +2,13 @@ package com.sudaustral.primerapropuesta;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +17,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//Pantalla completa
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);// NOTA:Este codigo elimina el titulo o el action bar en la APP
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); //Orientación Horizontal
         setContentView(R.layout.activity_main);
         WebView myWebView = (WebView) findViewById(R.id.webview);
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         myWebView.loadUrl("https://anin.users.earthengine.app/view/movil");
+        Toast.makeText(this,"Posicione su dispositivo de manera horizontal",   Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this,"Posicione su dispositivo de manera horizontal", Toast.LENGTH_LONG).show();
+        // La actividad está a punto de hacerse visible.
     }
 }
